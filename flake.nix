4316cab9d85d6a918aa3995c145b82a527eac19e
Sherif Abdalla <sherif@abdalla.uk>
2023-03-20 12:43:51 +0000

    Update main flake to support multi-cloud

--- flake.nix
+++ flake.nix
@@ -1,32 +1,89 @@
 {
-  description = "Nix flake dev environments";
+  description =
+    "Ready-made templates for easily creating flake-driven cloud environments";
 
-  outputs = { self }: {
-    templates = rec {
-      aws = {
-        path = ./aws;
-        description = "Amazon Web Services development environment";
-      };
-
-      azure = {
-        path = ./azure;
-        description = "Microsoft Azure development environment";
-      };
-
-      gcp = {
-        path = ./gcp;
-        description = "Google Cloud development environment";
-      };
-
-      digitalocean = {
-        path = ./digitalocean;
-        description = "Digital Ocean development environment";
-      };
-
-      linode = {
-        path = ./linode;
-        description = "Linode development environment";
-      };
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    {
+      templates = rec {
+        aws = {
+          path = ./aws;
+          description = "Amazon Web Services development environment";
+        };
+
+        azure = {
+          path = ./azure;
+          description = "Microsoft Azure development environment";
+        };
+
+        gcp = {
+          path = ./gcp;
+          description = "Google Cloud development environment";
+        };
+
+        digitalocean = {
+          path = ./digitalocean;
+          description = "Digital Ocean development environment";
+        };
+
+        linode = {
+          path = ./linode;
+          description = "Linode development environment";
+        };
+
+        # Aliases
+        # rt = rust-toolchain;
+      };
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        inherit (pkgs) mkShell writeScriptBin;
+        exec = pkg: "${pkgs.${pkg}}/bin/${pkg}";
+
+        format = writeScriptBin "format" ''
+          ${exec "nixpkgs-fmt"} **/*.nix
+        '';
+
+        dvt = writeScriptBin "dvt" ''
+          if [ -z $1 ]; then
+            echo "no template specified"
+            exit 1
+          fi
+
+          TEMPLATE=$1
+
+          ${exec "nix"} \
+            --experimental-features 'nix-command flakes' \
+            flake init \
+            --template \
+            "github:zerodeth/dev-templates#''${TEMPLATE}"
+        '';
+
+        update = writeScriptBin "update" ''
+          for dir in `ls -d */`; do # Iterate through all the templates
+            (
+              cd $dir
+              ${exec "nix"} flake update # Update flake.lock
+              ${exec "direnv"} reload    # Make sure things work after the update
+            )
+          done
+        '';
+      in
+      {
+        devShells = {
+          default = mkShell {
+            packages = [ format update ];
+          };
+        };
+
+        packages = rec {
+          default = dvt;
+
+          inherit dvt;
+        };
+      });
 }
